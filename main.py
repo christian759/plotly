0acd63d86176214af4bc97f1265478a32ba5635c
CEO1 <christian4onos@outlook.com>
2024-08-08 22:16:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import streamlit as st
 import re
 
 
-def formatter(equation:str, fig = 2) -> str:
-    pattern = r'(\d+)x'
-    output_string = re.sub(pattern, r'\1*x', equation)
-    formatted = output_string.replace("^", "**")
+def formatter(equation: str, fig: int = 2) -> str:
+    formatted = re.sub(r'(\d+)x', r'\1*x', equation)
+    formatted = formatted.replace("^", "**")
     formatted = formatted.replace("x", str(fig))
-    equa = eval(formatted)
-    return equa
+    equals = eval(formatted)
+    return equals
+
+def result():
+    ans = formatter(equation=equation)
+    return ans
 
 st.title("PLOTLY")
 st.subheader("Plot interactive graph from equations")
 
 st.markdown("blablablabla")
 equation = st.text_input(label= "enter an equation, pls ensure it is entered in the right format like the above examples")
-def result():
-    ans = formatter(equation=equation, fig=4)
-    return ans
 
 if equation:
-    st.markdown(result())
+    try:
+        st.markdown(result())
+    except Exception as e:
+        st.markdown(e)
 
